827556e174b68c83b57828c60911cb0cd8cdaf1d
EriickW <werick631@gmail.com>
2021-08-03 18:36:38 -0300

    First commit

---
+++ JavaScript/MultiplicaçãoSimples.js
@@ -0,0 +1,5 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+PROD = valor1 * valor2 
+let total = PROD; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);
